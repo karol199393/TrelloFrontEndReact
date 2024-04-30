2f10b92c5b9f6d0cea8e8d75e72f544b463ef7ea
Karol <114170012+karol199393@users.noreply.github.com>
2024-04-30 09:45:48 +0200

    start front

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="container1">
+        <h1>Trello</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
