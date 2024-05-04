11b8f0805a6509685b500dbbbde50d1e006be9b8
Karol <114170012+karol199393@users.noreply.github.com>
2024-05-04 11:49:17 +0200

    addTasksandConnectWithBackend

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, NavLink } from 'react-router-dom';
-import { Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Board from './components/board';
 import Tasks from './components/tasks';
 import Home from './components/home';
-import { Routes } from 'react-router-dom';
+import Login from './components/login';
+import Register from './components/register';
+
 
 
 function App() {
   return (
     <BrowserRouter>
-    <div>
-    <nav>
-      <ul>
-        <li><NavLink to ="/">Home</NavLink></li>
-        <li><NavLink to ="/board">Board</NavLink></li>
-        <li><NavLink to ="/tasks">Tasks</NavLink></li>
-      </ul>
-      </nav> 
-      <section>
-        <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/board"  element={<Board/>} />
-        <Route path="/tasks" element={<Tasks/>} />
-        </Routes>
-      </section>
-    </div>
+      <div>
+        <nav>
+          <ul>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/board">Board</NavLink></li>
+            <li><NavLink to="/tasks">Tasks</NavLink></li>
+            <li><NavLink to="/register">Register</NavLink></li> {/* nowy link */}
+            <li><NavLink to="/login">Login</NavLink></li> {/* nowy link */}
+          </ul>
+        </nav> 
+        <section>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/board" element={<Board />} />
+            <Route path="/tasks" element={<Tasks />} />
+            <Route path="/register" element={<Register />} /> {/* nowa ścieżka */}
+            <Route path="/login" element={<Login />} /> {/* nowa ścieżka */}
+          </Routes>
+        </section>
+      </div>
     </BrowserRouter>
-    
   );
 }
 
--- src/components/tasks.js
+++ src/components/tasks.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import './task.css';
 
 const Tasks = () => {
-     return (
-          <div>
-              <h1>Tasks</h1> 
-          </div>
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/v1/tasks')
+            .then(response => {
+                setTasks(response.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })      
+    }, []);
+
+     return ( 
+        <div className='table'>
+        <table>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                {tasks.map((task, index) => (
+                    <tr key={index}>
+                        <td>{task.title}</td>
+                        <td>{task.description}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
      )
 } 
 export default Tasks;
