d546cc036fb48add11391875be9f89f5c698fcc1
Karol <114170012+karol199393@users.noreply.github.com>
2024-05-04 14:33:21 +0200

    add login&register succesfull

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,55 @@
-import React from "react";
+import React ,{useState} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './login.css';
 
-const Login = () => {
+
+
+
+function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loginStatus, setLoginStatus] = useState("");
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const response = await fetch("http://localhost:8080/api/v1/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        });
+        const data = await response.json();
+        console.log(data);
+        if (response.ok) {
+            setLoginStatus("Logged in successfully");
+        } else {
+            setLoginStatus("Failed to log in");
+        }
+    };
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+       <form onSubmit={handleSubmit} className="login-form">
+           <label>
+               Username:
+               <input
+                   type="text"
+                   value={username}
+                   onChange={(event) => setUsername(event.target.value)}
+               />
+           </label>
+           <label>
+               Password:
+               <input
+                   type="password"
+                   value={password}
+                   onChange={(event) => setPassword(event.target.value)}
+               />
+           </label>
+           <button type="submit">Login</button>
+           <div>{loginStatus}</div> {/* Wyświetl status logowania */}
+       </form>
     );
 };
 
+
 export default Login;
-    
--- src/components/register.js
+++ src/components/register.js
@@ -1,10 +1,52 @@
-import React from "react";
+import React ,{ useState } from "react";
+import "./register.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Register = () => {
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const response = await fetch("http://localhost:8080/api/v1/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, username, password }),
+        });
+        const data = await response.json();
+        console.log(data);
+    };
     return (
-        <div>
-            <h1>Register</h1>
-        </div>
+      <form onSubmit={handleSubmit} className="register-form">
+      <label>
+        Email:
+        <input
+          type="text"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </label>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </label>
+      <button type="submit">Register</button>
+</form>
     );
 };
 
